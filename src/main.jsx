204f00f7af0d13cb761ab8215326cb1e8c8b1994
thanhlam <lamnguyen11e@gmail.com>
2022-01-21 18:12:14 +0700

    :construction: add starter page before add framer motion animation

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "typeface-ibm-plex-sans";
+import "./styles/App.scss";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </Router>,
+  document.getElementById("root")
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="banner">
+        <div className="inner-banner">
+          <div className="container">
+            <h1 className="main-headline small">Click on the menu button</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
